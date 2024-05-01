c3b4bdde3e9e55de6a817a78a0aa440151ff9767
pedronxw <pedroalmeidan@gmail.com>
2024-05-01 14:17:42 -0300

    Refactor Docker Compose file and remove RabbitMQ cluster service

--- src/domain/enterprise/events/index.ts
+++
@@ -1,6 +0,0 @@
-import { OnUserCreated } from '@/domain/application/subscribers/on-user-created'
-import { RabbitMQModule } from '@/infra/rabbitmq/rabbitmqModule'
-
-export const setupEvents = () => {
-  new OnUserCreated(new RabbitMQModule())
-}
